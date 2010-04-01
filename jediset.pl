6ce0da38b3c916dfc7cf39d3d76411224ab45216
valley <valley@valley.(none)>
2010-04-01 00:59:15 -0500

    Can print any number of columns and rows cleanly. Randomly generates shape fill and color. Number has yet to be implemented i.e. there is no concept of a card, or having multiple shapes on a card. Effectively, every card currently has only one shape, while this will soon move to 1, 2, or 3 shapes

--- jediset.pl
+++ jediset.pl
@@ -1,67 +1,87 @@
 #!/usr/bin/perl
 use strict; use warnings;
 use Term::ANSIColor;
-use subs qw(example_colors print_card);
-
-my $count = 0;
-
-my @color = qw(magenta  green  cyan);#black red green yellow blue magenta cyan white
-my @number= qw(1        2      3   );
-my @shape = qw(triangle rect   oval);
-my @fill  = qw(.        ~      @   );
+use subs qw(print_shapes shade colorize mold);
+#black red green yellow blue magenta cyan white
+my @color = qw(magenta  green  yellow);
+my @number= qw(1        2      3     );
+my @shape = qw(tria     rect   oval  );
+my @fill  = qw(`	+      @    );
 
 my @rect = qw(
-._______.
-|@@@@@@@|
-|@@@@@@@|
-|@@@@@@@|
-._______.
-);
-my @triangle = qw(
-....^....
-.../@\\...
-../@@@\\..
-./@@@@@\\.
-/_______\\
-);
+._____.
+|@@@@@|
+|@@@@@|
+|@@@@@|
+-------);
+my @tria = qw(
+..._...
+../@\\..
+./@@@\\.
+/@@@@@\\
+-------);
 my @oval = qw(
-.../-\\...
-./@@@@@\\.
-{@@@@@@@}
-.\\@@@@@/.
-...\\-/...
-);
-print $_."\n" for(@rect);
-print $_."\n" for(@triangle);
-print $_."\n" for(@oval);
+..___..
+./@@@\\.
+\(@@@@@\)
+.\\@@@/.
+..---..);
 
+my @form = @rect;#the current shape used for the next card to print
+
+my @deck;
+
+my $columns = 12;
+my $rows = 1;
+
+my $s; my @s;#shape
+my $f; my @f;#fill
+my $c; my @c;#color
+
+for(1..$rows){
+for(1..$columns){
+	my $count = 0;
+	do{
+		exit 0 if $count++ > @shape*@fill*@color;
+		$s = int rand @shape;
+		$f = int rand @fill;
+		$c = int rand @color;
+	}while(defined $deck[$s][$f][$c]);
+	$deck[$s][$f][$c]=0;
+	push @s, $s; push @f, $f; push @c, $c;
+}
+
+for my $i (0..$#form){
+for(0..$#s){
+	mold $s[$_];
+	shade $f[$_];
+	colorize $c[$_];
+	print $form[$i]." ";
+}
+print "\n";
+}
+undef @s;undef @f;undef @c;
+}
+
+#Put a shape in @form, randomly or by index into @shape from argument
+sub mold{
+	my $s = shift; $s = int rand @shape unless defined $s;
+	@form = @tria if($shape[$s] eq 'tria');
+	@form = @rect if($shape[$s] eq 'rect');
+	@form = @oval if($shape[$s] eq 'oval');
+	return $s;
+}
+
+#Set the fill to random, or from @fill indexed by first argument
 sub shade{
-	my $f = shift;
-	$_ =~ s/[@.~]/$fill[$f]/g for(@rect);
-	$_ =~ s/[@.~]/$fill[$f]/g for(@triangle);
-	$_ =~ s/[@.~]/$fill[$f]/g for(@oval);
-}
-sub print_card{
-#Variable values try to get rand to generate based on length of array
-my $c = shift; $c = int(rand(3)) unless defined $c; #color
-my $n = shift; $n = int(rand(3)) unless defined $n; #number
-my $s = shift; $s = int(rand(3)) unless defined $s; #shape
-my $f = shift; $f = int(rand(3)) unless defined $f; #fill
-
-print color "bold $color[$c]";
-print "$shape[$s] filled with $fill[$f]\n" for(1..$number[$n]);
-print color 'reset';
+	my $f = shift; $f = int rand @fill unless defined $f;
+	$_ =~ s/[@fill]/$fill[$f]/g for(@form);
+	return $f;
 }
 
-sub example_colors{
-my $message = shift;
-print color 'reset';
-print color 'bold';
-for (@color){
-	print color $_;
-	print $_ unless $message;
-	if ($message){for(@shape){print $_."\n";}}
-	print "\n";
-}
-print color 'reset';
+#Set the color to random, or from @color indexed by first argument
+sub colorize{
+	my $c = shift; $c = int rand @color unless defined $c;
+	print color "bold $color[$c]";
+	return $c;
 }
